2aee1f546d64012e915ebba5321bdac7c6ad8e93
Rachel <rawilwgu@Galadriel>
2024-07-06 16:57:30 -0500

    Step 4: Unit Tests written and passed

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,52 @@
 import pytest
-# TODO: add necessary import
-
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+import pandas as pd
+from ml.model import train_model, compute_model_metrics
+from ml.data import process_data
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
+def test_model_instance():
+    '''
+    Tests to confirm that the model is an instance of the given model class.
+    '''
+    X_dummy = pd.DataFrame({'feature_1': [1, 2, 3], 'feature_2': ['A', 'B', 'C']})
+    y_dummy = pd.Series([0, 1, 0])
+
+    X_dummy_encoded = pd.get_dummies(X_dummy)
+
+    model = train_model(X_dummy_encoded, y_dummy)
+
+    assert isinstance(model, RandomForestClassifier)
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_metrics():
     """
-    # add description for the third test
+    Tests to confirm that the computing_model_metrics function works correctly with expected values.
     """
-    # Your code here
-    pass
+    y_true = pd.Series([0, 1, 0, 1])
+    y_pred = pd.Series([0, 0, 1, 1])
+    precision, recall, f1 = compute_model_metrics(y_true, y_pred)
+
+    assert precision == 0.5
+    assert recall == 0.5
+    assert f1 == 0.5
+
+
+def test_train_dataset():
+    """
+    Tests to confirm that the dataset shapes are the correct format.
+    """
+
+    data = pd.DataFrame({'feature_1': [1, 2, 3],
+                        'feature_2': ['A', 'B', 'C'],
+                        'label': [0, 1, 0]
+                         }
+                        )
+
+    train_data, test_data = train_test_split(data, test_size=0.2)
+
+    assert len(train_data) > 0
+    assert len(test_data) > 0
+    assert isinstance(train_data, pd.DataFrame)
+    assert isinstance(test_data, pd.DataFrame)
