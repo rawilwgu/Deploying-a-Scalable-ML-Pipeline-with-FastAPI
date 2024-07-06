86c06a2e5e0374239042b85892087595b0d7f449
Rachel Wilson <rachel.wilson.rawil9@gmail.com>
2024-07-06 14:37:50 -0500

    Update train_model.py, project_path

--- train_model.py
+++ train_model.py
@@ -14,7 +14,7 @@ from ml.model import (
 )
 
 # TODO: load the cencus.csv data
-project_path = "\wsl.localhost\Ubuntu\home\rawilwgu\Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
+project_path = "\Ubuntu\home\rawilwgu\Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
 data = pd.read_csv(data_path)
