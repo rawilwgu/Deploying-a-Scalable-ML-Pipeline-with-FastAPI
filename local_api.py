3b6f6f4cfa4966cfd5776425c2e5dab0abbcdf2b
Rachel <rawilwgu@Galadriel>
2024-07-06 19:12:35 -0500

    Update local_api.py with GET and POST requests

--- local_api.py
+++ local_api.py
@@ -3,12 +3,13 @@ import json
 import requests
 
 # TODO: send a GET using the URL http://127.0.0.1:8000
-r = # Your code here
+r = requests.get("http://127.0.0.1:8000")
 
 # TODO: print the status code
-# print()
+print("GET Status Code:", r.status_code)
 # TODO: print the welcome message
-# print()
+if r.status_code == 200:
+    print("GET Response:", r.json()["message"])
 
 
 
@@ -30,9 +31,10 @@ data = {
 }
 
 # TODO: send a POST using the data above
-r = # Your code here
+r = requests.post("http://127.0.0.1:8000/data/", json=data)
 
 # TODO: print the status code
-# print()
+print("\nPOST Status Code:", r.status_code)
 # TODO: print the result
-# print()
+if r.status_code == 200:
+    print("POST Result:", r.json()["result"])
