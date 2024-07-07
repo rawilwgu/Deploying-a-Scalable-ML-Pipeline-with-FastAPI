b32deee44cbb87450f240204d7a11eb2f0329b07
Rachel <rawilwgu@Galadriel>
2024-07-06 19:22:06 -0500

    Update data.py line 60 to fix AttributeError

--- ml/data.py
+++ ml/data.py
@@ -57,7 +57,7 @@ def process_data(
         encoder = OneHotEncoder(sparse=False, handle_unknown="ignore")
         lb = LabelBinarizer()
         X_categorical = encoder.fit_transform(X_categorical)
-        y = lb.fit_transform(y.values).ravel()
+        y = lb.fit_transform(y).ravel() # y = lb.fit_transform(y.values).ravel()
     else:
         X_categorical = encoder.transform(X_categorical)
         try:
